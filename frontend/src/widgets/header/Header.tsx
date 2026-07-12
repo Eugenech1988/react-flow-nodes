1246a990877843851c68b8a740ca42e23e1e6faa
Eugenech1988 <eugenech1988@gmail.com>
2026-07-12 15:24:22 +0300

    minor fix

--- frontend/src/widgets/header/Header.tsx
+++ frontend/src/widgets/header/Header.tsx
@@ -6,7 +6,7 @@ export const Header = () => {
   return (
     <header className="flex items-center justify-between px-6 py-3.5 bg-[#f1f5f9] dark:bg-[#030712] border-b border-border/80 shadow-xs z-40 shrink-0 transition-colors duration-300">
       <div className="flex items-center gap-3 select-none">
-        <div data-testid="app-logo" className="flex items-center justify-center w-10 h-10 rounded-full shrink-0 select-none overflow-hidden bg-white dark:bg-black border border-border/20">
+        <div data-testid="app-logo" className="flex items-center justify-center w-10 h-10 rounded-full shrink-0 select-none overflow-hidden bg-black border border-border/20">
           <svg viewBox="0 0 200 200" fill="none" xmlns="http://www.w3.org/2000/svg" className="w-full h-full p-1.5">
             <path
               d="M60 140V60H100C122.091 60 140 77.9086 140 100C140 122.091 122.091 140 100 140H60Z"
