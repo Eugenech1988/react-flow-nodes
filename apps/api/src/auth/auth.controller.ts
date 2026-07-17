e16aeced98077967bcf7f7d35ee42546e99041b8
Eugenech1988 <eugenech1988@gmail.com>
2026-07-17 14:54:26 +0300

    minor be fix

--- apps/api/src/auth/auth.controller.ts
+++ apps/api/src/auth/auth.controller.ts
@@ -1,9 +1,10 @@
-import { Controller, Post, Body, Res, UseGuards } from '@nestjs/common';
+import { Controller, Post, Body, Res, UseGuards, Get, Req } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { RegisterDto } from './dtos/register.dto';
-import type { Response } from 'express';
+import type { Response, Request } from 'express';
 import { AuthGuard } from '@nestjs/passport';
 import { CurrentUser } from 'src/utils/decorators/current-user.deacorator';
+import { GoogleOauthGuard } from './guards/google.guard';
 
 @Controller('auth')
 export class AuthController {
@@ -34,4 +35,18 @@ export class AuthController {
   async logout(@Res({ passthrough: true }) res: Response) {
     res.cookie('refreshToken', '');
   }
+
+  @Get('gooogle')
+  @UseGuards(GoogleOauthGuard)
+  async googleAuth(@Req() req: Request) {
+    console.log(req.user);
+    // const user = req.user;
+    //
+    // return {
+    //   message: 'Google auth success',
+    //   user,
+    // };
+  }
+
+
 }
--- apps/api/src/auth/auth.module.ts
+++ apps/api/src/auth/auth.module.ts
@@ -1,26 +1,35 @@
 import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
 import { UsersModule } from '../users/users.module';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
-import { JwtModule } from '@nestjs/jwt';
-import { ConfigService } from '@nestjs/config';
 import { LocalStrategy } from './strategies/local.strategy';
 import { JwtRefreshStrategy } from './strategies/jwt.refresh.strategy';
+import { GoogleStrategy } from './strategies/google.strategy';
 
 @Module({
   imports: [
     UsersModule,
+    PassportModule.register({defaultStrategy: 'google'}),
     JwtModule.registerAsync({
       inject: [ConfigService],
       useFactory: async (configService: ConfigService) => ({
         secret: configService.getOrThrow('JWT_ACCESS_SECRET'),
         signOptions: {
-          expiresIn: configService.getOrThrow('JWT_ACCESS_EXPIRES'),
-        },
-      }),
-    }),
+          expiresIn: configService.getOrThrow('JWT_ACCESS_EXPIRES')
+        }
+      })
+    })
   ],
   controllers: [AuthController],
-  providers: [AuthService, LocalStrategy, JwtRefreshStrategy],
+  providers: [
+    AuthService,
+    LocalStrategy,
+    JwtRefreshStrategy,
+    GoogleStrategy
+  ]
 })
-export class AuthModule {}
+export class AuthModule {
+}
