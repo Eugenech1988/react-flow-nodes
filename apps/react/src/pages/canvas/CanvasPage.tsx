d3fa3d09c8dc80df14c6b9ee98db0a6a51826d10
Eugenech1988 <eugenech1988@gmail.com>
2026-07-27 12:23:41 +0300

    root page fix

--- apps/react/src/pages/canvas/CanvasPage.tsx
+++ apps/react/src/pages/canvas/CanvasPage.tsx
@@ -1,7 +1,19 @@
 import { NodesToolbar } from '@/widgets/nodes-toolbar';
 import { Canvas } from '@/widgets/canvas';
+import { usePipelines } from '@/shared/hooks';
+import { GlobalLoader } from '@/shared/ui';
+import { Navigate } from 'react-router-dom';
 
 export const CanvasPage = () => {
+  const { pipelines, isLoading } = usePipelines();
+
+  if (isLoading) {
+    return <GlobalLoader />
+  }
+
+  if (!pipelines || pipelines.length === 0) {
+    return <Navigate to="/pipelines" replace />;
+  }
   return (
     <div className="flex flex-col h-full w-full overflow-hidden">
       <NodesToolbar/>
